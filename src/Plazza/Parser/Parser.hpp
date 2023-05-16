75d6b8afdb4d4466e2d92be21303ca4bdfc07440
hugon <hugo.noury@epitech.eu>
2023-05-16 19:40:01 +0200

    [ADD]: string vector that stores authorized inputs + Parser class inheriting from Parser Interface

---
+++ src/Plazza/Parser/Parser.hpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2023
+** plazza
+** File description:
+**
+*/
+
+#pragma once
+#include "IParser.hpp"
+#include <regex>
+
+std::vector<std::string> authorizedInputs =
+    {"regina",
+     "margarita",
+     "americana",
+     "fantasia",
+     "S",
+     "M",
+     "L",
+     "XL",
+     "XXL"};
+
+class Parser : public IParser {
+public:
+    Parser(int ac, const char **av);
+    ~Parser() override = default;
+    std::vector<std::unique_ptr<IPizza>> getInput() override;
+    std::string getInputString(std::string input, int i);
+    bool isAuthorized(std::string inputParam);
+private:
+    std::vector<std::string> _input;
+    std::vector<std::string> _args;
+protected:
+};
