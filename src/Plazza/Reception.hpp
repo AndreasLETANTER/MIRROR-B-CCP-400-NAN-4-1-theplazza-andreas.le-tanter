68221b5b19f82bd1c3dbc212bd2de11154488793
Luchooooo <louis.barreteau@epitech.eu>
2023-05-17 10:52:01 +0200

    [ADD]: sendPizzaToQuee and waitCommands functions

--- src/Plazza/Reception.hpp
+++ src/Plazza/Reception.hpp
@@ -7,32 +7,47 @@
 
 #pragma once
 
+#include "IReception.hpp"
+#include "SafeQueue.hpp"
+
 /**
  * @brief Interface for the reception class
 */
-class IReception {
+class Reception : public IReception {
     public:
-        virtual ~IReception() = default;
+        /**
+         * @brief Constructor of the reception class
+         * @details this function create a reception with a parser and a safe queue
+        */
+        Reception();
+        /**
+         * @brief Destructor of the reception class
+         * @details this function destroy the reception
+        */
+        ~Reception();
         /**
          * @brief Wait for commands from the user
          * @details this function is the main loop that use the parser to get the pizza to cook in a vector
         */
-        virtual void waitCommands() = 0;
+        void waitCommands() override;
         /**
          * @brief Send the pizza to the queue
          * @details this function send the pizza to safe queue for the kitchen
         */
-        virtual void sendPizzaToQueue(std::shared_ptr<IPizza> pizza) = 0;
+        void sendPizzaToQueue(std::shared_ptr<IPizza> pizza) override;
         /**
          * @brief Create a kitchen
          * @details this function create a kitchen with a cook and a pantry
         */
-        virtual void createKitchen() = 0;
+        void createKitchen() override;
         /**
          * @brief send the pizza to be cooked in a kitchen
         */
-       virtual void sendPizzaToKitchen() = 0;
+        void sendPizzaToKitchen() override;
 
     protected:
     private:
+        bool m_is_running;
+        std::string m_command;
+        SafeQueue<std::shared_ptr<IPizza>> m_queue;
 };
