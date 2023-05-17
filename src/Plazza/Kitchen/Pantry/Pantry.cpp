dc84a935e9d2e261b3b4324b64f1f31451620aa1
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-05-17 18:55:45 +0200

    [ADD]: documentation on every implementation of Pantry Class

--- src/Plazza/Kitchen/Pantry/Pantry.cpp
+++ src/Plazza/Kitchen/Pantry/Pantry.cpp
@@ -20,11 +20,21 @@ Pantry::Pantry()
     m_pantry[PizzaIngredient::ChiefLove] = 5;
 }
 
+/**
+ * @brief Add N ingredient to the pantry
+ * @param t_ingredient the ingredient to add
+ * @param t_nb_ingredient the number of ingredient to add
+*/
 void Pantry::addIngredient(PizzaIngredient t_ingredient, int t_nb_ingredient)
 {
     m_pantry[t_ingredient] += t_nb_ingredient;
 }
 
+/**
+ * @brief Remove N ingredient from the pantry
+ * @param t_ingredient the ingredient to remove
+ * @param t_nb_ingredient the number of ingredient to remove
+*/
 void Pantry::removeIngredient(PizzaIngredient t_ingredient, int t_nb_ingredient)
 {
     m_pantry[t_ingredient] -= t_nb_ingredient;
@@ -33,6 +43,12 @@ void Pantry::removeIngredient(PizzaIngredient t_ingredient, int t_nb_ingredient)
         m_pantry[t_ingredient] = 0;
 }
 
+/**
+ * @brief Check if the pantry contain the ingredient
+ * @param t_ingredient the ingredient to check
+ * @return true if the pantry contain the ingredient
+ * @return false if the pantry doesn't contain the ingredient
+*/
 bool Pantry::checkIngredient(PizzaIngredient t_ingredient)
 {
     if (m_pantry[t_ingredient] > 0)
