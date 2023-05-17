0e3ebcb20ccccd744345f885aa51446188853f0d
hugon <hugo.noury@epitech.eu>
2023-05-17 11:06:56 +0200

    [FIX]: i parameter type in getInputString() + [MOVE]: authorizedInputs in Parser class

--- src/Plazza/Parser/Parser.hpp
+++ src/Plazza/Parser/Parser.hpp
@@ -9,26 +9,25 @@
 #include "IParser.hpp"
 #include <regex>
 
-std::vector<std::string> authorizedInputs =
-    {"regina",
-     "margarita",
-     "americana",
-     "fantasia",
-     "S",
-     "M",
-     "L",
-     "XL",
-     "XXL"};
-
 class Parser : public IParser {
 public:
     Parser(int ac, const char **av);
     ~Parser() override = default;
     std::vector<std::unique_ptr<IPizza>> getInput() override;
-    std::string getInputString(std::string input, int i);
+    std::string getInputString(std::string input, int *i);
     bool isAuthorized(std::string inputParam);
 private:
     std::vector<std::string> _input;
     std::vector<std::string> _args;
+    std::vector<std::string> authorizedInputs =
+        {"regina",
+         "margarita",
+         "americana",
+         "fantasia",
+         "S",
+         "M",
+         "L",
+         "XL",
+         "XXL"};
 protected:
 };
