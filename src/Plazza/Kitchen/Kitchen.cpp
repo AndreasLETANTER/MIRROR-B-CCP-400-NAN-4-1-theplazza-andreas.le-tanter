226203b163680935b6ca322b812b72744ecb43a8
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-05-19 12:45:16 +0200

    [ADD]: refill time for the pantry

--- src/Plazza/Kitchen/Kitchen.cpp
+++ src/Plazza/Kitchen/Kitchen.cpp
@@ -8,13 +8,34 @@
 #include "Kitchen.hpp"
 #include "Cook/CookRoutine.hpp"
 
-Kitchen::Kitchen(int t_nbCook, double t_timeMultiplier)
+static void PantryRoutine(std::shared_ptr<IPantry> t_pantry, int t_timeMilliseconds, std::shared_ptr<IMutex> t_mutex)
+{
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(t_timeMilliseconds));
+        t_mutex->lock();
+        t_pantry->addIngredient(PizzaIngredient::Dough, 1);
+        t_pantry->addIngredient(PizzaIngredient::Tomato, 1);
+        t_pantry->addIngredient(PizzaIngredient::Gruyere, 1);
+        t_pantry->addIngredient(PizzaIngredient::Ham, 1);
+        t_pantry->addIngredient(PizzaIngredient::Mushrooms, 1);
+        t_pantry->addIngredient(PizzaIngredient::Steak, 1);
+        t_pantry->addIngredient(PizzaIngredient::Eggplant, 1);
+        t_pantry->addIngredient(PizzaIngredient::GoatCheese, 1);
+        t_pantry->addIngredient(PizzaIngredient::ChiefLove, 1);
+        t_mutex->unlock();
+    }
+}
+
+Kitchen::Kitchen(int t_nbCook, double t_timeMultiplier, int t_refillTime)
 {
     m_nbCook = t_nbCook;
     m_timeMultiplier = t_timeMultiplier;
     m_nbPizzaMax = t_nbCook * 2;
     m_nbCurrentPizza = 0;
+    m_refillTime = t_refillTime;
+    m_pantryMutex = std::make_shared<Mutex>();
     createPantry();
+    m_pantryThread = std::make_unique<Thread<decltype(PantryRoutine), decltype(m_pantry), decltype(m_refillTime), decltype(m_pantryMutex)>>(PantryRoutine, m_pantry, m_refillTime, m_pantryMutex);
     createCooks();
 }
 
@@ -34,15 +55,19 @@ void Kitchen::createCooks()
 
 void Kitchen::createPantry()
 {
-    m_pantry = std::make_unique<Pantry>();
+    m_pantry = std::make_shared<Pantry>();
 }
 
 bool Kitchen::checkPantry(std::vector<PizzaIngredient> t_ingredientNeeded)
 {
+    m_pantryMutex->lock();
     for (auto ingredient : t_ingredientNeeded) {
-        if (!m_pantry->checkIngredient(ingredient))
+        if (!m_pantry->checkIngredient(ingredient)) {
+            m_pantryMutex->unlock();
             return false;
+        }
     }
+    m_pantryMutex->unlock();
     return true;
 }
 
@@ -57,7 +82,9 @@ void Kitchen::addPizzaToPool(std::shared_ptr<IPizza> t_pizza)
 {
     m_pizzaPool->push(t_pizza);
     m_nbCurrentPizza++;
+    m_pantryMutex->lock();
     for (auto &ingredient : t_pizza->getIngredients()) {
         m_pantry->removeIngredient(ingredient, 1);
     }
+    m_pantryMutex->unlock();
 }
--- src/Plazza/Kitchen/Kitchen.hpp
+++ src/Plazza/Kitchen/Kitchen.hpp
@@ -14,7 +14,7 @@
 class Kitchen : public IKitchen {
     public:
         Kitchen() = default;
-        Kitchen(int t_nbCook, double t_timeMultiplier);
+        Kitchen(int t_nbCook, double t_timeMultiplier, int t_refillTime);
         ~Kitchen() override;;
         void createCooks() override;
         void createPantry() override;
@@ -24,10 +24,14 @@ class Kitchen : public IKitchen {
 
     protected:
     private:
-        std::unique_ptr<IPantry> m_pantry;
+        int m_refillTime;
+        std::shared_ptr<IPantry> m_pantry;
         std::shared_ptr<IMutex> _mutex = std::make_shared<Mutex>();
         std::shared_ptr<ISafeQueue<std::shared_ptr<IPizza>>> m_pizzaPool = std::make_shared<SafeQueue<std::shared_ptr<IPizza>>>(_mutex);
         std::vector<std::unique_ptr<IThread>> m_cookPool;
+        std::unique_ptr<IThread> m_pantryThread;
+        std::shared_ptr<IMutex> m_pantryMutex = std::make_shared<Mutex>();
+        std::unique_ptr<IThread> m_kitchenBodyguard;
         double m_timeMultiplier;
         int m_nbCook;
         int m_nbPizzaMax;
