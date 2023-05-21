07bc42dbd18ce6c60dc8b02c5f58af1cd4eb785a
hugon <hugo.noury@epitech.eu>
2023-05-21 15:52:41 +0200

    [REFACTOR]: authorizedInputs vector in authorizedPizzas and authorizedSizes vectors + [REMOVE]: useless private members + [ADD]: setPizzas(), setPizza(), isInVector() and stringToPizzaSize()

--- src/Plazza/Parser/Parser.hpp
+++ src/Plazza/Parser/Parser.hpp
@@ -14,17 +14,20 @@ public:
     Parser() = default;
     ~Parser() override = default;
     std::vector<std::shared_ptr<IPizza>> getInput() override;
+    std::vector<std::shared_ptr<IPizza>> setPizzas(std::vector<std::string> inputs);
+    std::shared_ptr<IPizza> setPizza(const std::string& pizzaName, const std::string& pizzaSize);
     std::string getInputString(std::string input, int *i);
-    bool isAuthorized(std::string inputParam);
+    bool isAuthorized(const std::string& inputParam);
+    bool isInVector(std::vector<std::string> t_vector, const std::string& str_to_find);
+    PizzaSize stringToPizzaSize(const std::string& str);
 private:
-    std::vector<std::string> _input;
-    std::vector<std::string> _args;
-    std::vector<std::string> authorizedInputs =
+    std::vector<std::string> authorizedPizzas =
         {"regina",
          "margarita",
          "americana",
-         "fantasia",
-         "S",
+         "fantasia"};
+    std::vector<std::string> authorizedSizes =
+        {"S",
          "M",
          "L",
          "XL",
