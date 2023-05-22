95e3fe9a80348aefcdd2d0796476f53ae61449b1
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-05-22 13:03:46 +0200

    [ADD]: documentation on ISafeQueue class

--- src/ISafeQueue/SafeQueue.hpp
+++ src/ISafeQueue/SafeQueue.hpp
@@ -22,11 +22,45 @@
 template <typename T>
 class SafeQueue : public ISafeQueue<T> {
     public:
+        /**
+         * @brief Construct a new Safe Queue< T>:: Safe Queue object
+         * @details Construct a new Safe Queue< T>:: Safe Queue object and initialize the mutex
+         * @tparam T type of the queue
+         * @param t_mutex mutex to use
+        */
         SafeQueue(std::shared_ptr<IMutex> t_mutex);
+        /**
+         * @brief Destroy the Safe Queue< T>:: Safe Queue object
+         * @details Destroy the Safe Queue< T>:: Safe Queue object and clear the queue
+         * @tparam T type of the queue
+        */
         ~SafeQueue();
+        /**
+         * @brief push a value in the queue
+         * @tparam T type of the queue
+         * @param value value to push
+        */
         void push(T value) override;
+        /**
+         * @brief try to pop a value from the queue
+         * @tparam T type of the queue
+         * @param value value to pop
+         * @return true if the queue is not empty
+         * @return false if the queue is empty
+        */
         bool tryPop(T &value) override;
+        /**
+         * @brief get the size of the queue
+         * @tparam T type of the queue
+         * @return size_t size of the queue
+        */
         size_t size() override;
+        /**
+         * @brief pop a value from the queue
+         * @details pop a value from the queue and wait if the queue is empty until a value is pushed
+         * @tparam T type of the queue
+         * @return T value
+        */
         T pop() override;
 
     protected:
