ef24c3a0397403761f3d8adc0a13a88549280d6c
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-05-22 10:33:56 +0200

    [ADD]: ITimer interface for Timer class

--- src/Timer/ITimer.hpp
+++ src/Timer/ITimer.hpp
@@ -1,58 +1,50 @@
 /*
 ** EPITECH PROJECT, 2023
-** MIRROR-B-OOP-400-NAN-4-1-arcade-andreas.le-tanter [WSL: Ubuntu]
+** MIRROR-B-CCP-400-NAN-4-1-theplazza-andreas.le-tanter
 ** File description:
-** Timer
+** ITimer
 */
 
 #pragma once
-#include <chrono>
-#include "ITimer.hpp"
 
-/**
- * @brief Timer class
-*/
-class Timer : public ITimer {
+class ITimer {
     public:
-        Timer() = default;
-        ~Timer() = default;
+        virtual ~ITimer() = default;
         /**
          * @brief Start the timer
          * @details Start the timer and set the _is_started boolean to true
         */
-        void StartTimer() override;
+        virtual void StartTimer() = 0;
         /**
          * @brief Get the Elapsed Time object
          * @details Get the elapsed time in seconds
          * @return double
         */
-        double GetElapsedTime() override;
+        virtual double GetElapsedTime() = 0;
         /**
          * @brief Get the Elapsed Time In Milli Seconds object
          * @details Get the elapsed time in milliseconds
          * @return double
         */
-        double GetElapsedTimeInMilliSeconds() override;
+        virtual double GetElapsedTimeInMilliSeconds() = 0;
         /**
          * @brief Get the Elapsed Time In Micro Seconds object
          * @details Get the elapsed time in microseconds
          * @return double
         */
-        double GetElapsedTimeInMicroSeconds() override;
+        virtual double GetElapsedTimeInMicroSeconds() = 0;
         /**
          * @brief Reset the timer
          * @details Reset the timer and set the _is_started boolean to false
         */
-        void ResetTimer() override;
+        virtual void ResetTimer() = 0;
         /**
          * @brief Check if the timer is started
          * @return true if the timer is started
          * @return false if the timer is not started 
          */
-        bool IsStarted() override { return m_is_started; };
+        virtual bool IsStarted() = 0;
 
     protected:
     private:
-        std::chrono::time_point<std::chrono::system_clock> m_start;
-        bool m_is_started = false;
 };
