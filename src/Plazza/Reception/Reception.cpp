d98d944113e64504ea6d360c276837ec79f82b19
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-05-25 15:51:46 +0200

    [ADD]: basic implementation of the IPC

--- src/Plazza/Reception/Reception.cpp
+++ src/Plazza/Reception/Reception.cpp
@@ -29,6 +29,8 @@ Reception::Reception(double multiplier, unsigned int nbCooks, unsigned int refil
     m_Segment = boost::interprocess::managed_shared_memory(boost::interprocess::open_or_create, "SharedMemory", 65536);
     const m_ShmemAllocator alloc_inst(m_Segment.get_segment_manager());
     m_sharedKitchensVector = m_Segment.construct<m_SharedVector>("KitchensVector")(alloc_inst);
+    m_sharedPizza = m_Segment.construct<std::shared_ptr<IKitchen>>("SharedPizza")(nullptr);
+    m_nbKitchens = 0;
     waitCommands();
 }
 
@@ -40,6 +42,7 @@ Reception::~Reception()
 {
     m_file.close();
     m_Segment.destroy<m_SharedVector>("KitchensVector");
+    m_Segment.destroy<std::shared_ptr<IKitchen>>("SharedPizza");
     boost::interprocess::shared_memory_object::remove("SharedMemory");
 }
 
@@ -85,10 +88,25 @@ void Reception::createKitchen()
     pid_t pid = fork();
 
     if (pid == 0) {
-        m_mutex.lock();
+        bool isKitchenFilled = false;
         m_SharedVector* childVector = m_Segment.find<m_SharedVector>("KitchensVector").first;
-        childVector->push_back(std::make_unique<Kitchen>(m_nbCooks, m_multiplier, m_refillTime));
+        std::shared_ptr<IKitchen> kitchen = std::make_shared<Kitchen>(m_nbCooks, m_multiplier, m_refillTime);
+        size_t id = m_nbKitchens;
+        m_nbKitchens++;
+        m_mutex.lock();
+        childVector->push_back(isKitchenFilled);
         m_mutex.unlock();
+        while (kitchen->isExitNeeded()) {
+            m_mutex.lock();
+            std::shared_ptr<IPizza>* pizza = m_Segment.find<std::shared_ptr<IPizza>>("SharedPizza").first;
+            if (pizza && kitchen->isKitchenFilled() == false && kitchen->checkPantry((*pizza)->getIngredients()) == true) {
+                kitchen->addPizzaToPool(*pizza);
+                *pizza = nullptr;
+            }
+            childVector[id][0] = kitchen->isKitchenFilled();
+            m_mutex.unlock();
+        }
+        m_nbKitchens--;
         exit(0);
     } else if (pid == -1) {
         std::cerr << "Error while creating kitchen" << std::endl;
@@ -99,10 +117,13 @@ void Reception::sendPizzaToKitchen(std::shared_ptr<IPizza> t_pizza)
 {
     m_mutex.lock();
     m_sharedKitchensVector = m_Segment.find<m_SharedVector>("KitchensVector").first;
+    std::shared_ptr<IPizza>* pizza = m_Segment.find<std::shared_ptr<IPizza>>("SharedPizza").first;
 
-    for (auto &kitchen : *m_sharedKitchensVector) {
-        if (kitchen->isKitchenFilled() == false && kitchen->checkPantry(t_pizza->getIngredients()) == true) {
-            kitchen->addPizzaToPool(t_pizza);
+    *pizza = t_pizza;
+    usleep(3000);
+    for (size_t i = 0; i < m_nbKitchens; i++) {
+        if (m_sharedKitchensVector[i][0] == false) {
+            m_sharedKitchensVector[i][0] = true;
             m_mutex.unlock();
             return;
         }
