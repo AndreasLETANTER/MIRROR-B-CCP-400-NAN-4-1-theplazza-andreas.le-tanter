b4f6ffdb1c8660c676f0d4b0882a20b124abd37b
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-05-25 20:57:56 +0200

    [ADD]: include for condition_variable in IConditionVariable class

--- src/IConditionVariable/IConditionVariable.hpp
+++ src/IConditionVariable/IConditionVariable.hpp
@@ -7,6 +7,7 @@
 
 #pragma once
 #include <mutex>
+#include <condition_variable>
 
 class IConditionVariable {
     public:
---
+++ src/ISharedMemory/ISharedMemory.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2023
+** MIRROR-B-CCP-400-NAN-4-1-theplazza-andreas.le-tanter
+** File description:
+** ISharedMemory
+*/
+
+#pragma once
+
+template<typename T>
+class ISharedMemory {
+    public:
+        virtual ~ISharedMemory() = default;
+        virtual void createSharedMemory(std::string t_memoryName, size_t t_memorySize) = 0;
+        virtual void constructSegment(std::string t_segmentName) = 0;
+        virtual void destroySegment(std::string t_segmentName) = 0;
+        virtual void destroySharedMemory(std::string t_memoryName) = 0;
+        virtual T getSegment(std::string t_segmentName) = 0;
+
+    protected:
+    private:
+};
